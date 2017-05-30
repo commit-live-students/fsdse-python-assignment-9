39e371b146c7bbce021cb12dfb61b2fe8f212286
sangam <sangam.angre@gmail.com>
2017-05-30 11:22:08 +0530

    test cases created

--- tests/test_validate_email.py
+++ tests/test_validate_email.py
@@ -11,20 +11,12 @@ class TestValidate_email(TestCase):
             self.assertFalse(no_function_found("validate_email"))
 
         check = False
-        tree = ast.parse(''.join(open("../email.py")))
-        for node in tree.body:
-            if isinstance(node, ast.FunctionDef):
-                if node.name == "validate_email":
-                    for nodelist in node.body:
-                        if hasattr(nodelist, 'targets'):
-                            i = 0
-                            for target in nodelist.targets:
-                                if target.id == "pattern":
-                                    self.assertEqual("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", nodelist.value.args[i].s)
-                                    check = True
-                                    break
-                                i = i + 1
-        if check:
-            self.assertTrue(succeed("test cases are passed"))
-        else:
-            self.assertFalse(no_function_found("pattern keyword"))
+
+        s = "sangam@greyatom.com"
+
+        solution = validate_email(s)
+
+
+        self.assertNotEqual(None, solution)
+
+        self.assertEqual(True, solution)
