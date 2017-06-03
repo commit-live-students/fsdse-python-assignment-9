47dbca342215416a287a28679ae7259011e995b4
nikhilakki <nikhilakki@users.noreply.github.com>
2017-06-03 10:18:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,10 @@
 
+import re
+def validate_email(str1="default"):
+    pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    if not re.search(pattern, str1):
+        return False
+    else:
+        return True
+
+print(validate_email("nikhil@gmail.com"))
