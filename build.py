6710d67e6f4445530550851812a671896a12a251
coded9 <coded9@users.noreply.github.com>
2017-06-11 10:12:44 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,10 @@
-
+import re
+def validate_email(str):
+    if not str:
+        return False
+    else:
+        if re.search("[A-Za-z0-9_.]+@[A-Za-z0-9]+\\.[A-Za-z]{2,5}$",str) != None:
+            return True
+        else:
+            return False
+#print validate_email("mkyong@gmail.com.1a")
