84129053211fd89af8b6ec41c3f1541097cabbef
utk1801 <utk1801@users.noreply.github.com>
2017-06-14 12:23:08 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,10 @@
-
+import re
+def validate_email(user_input):
+    if user_input==None:
+        return False
+    isvalid=re.match(r'[\w.-]+@[\w.-]+.\w+',user_input)
+    if isvalid:
+        return True
+    else:
+        return False
+print validate_email("heha@xya")
