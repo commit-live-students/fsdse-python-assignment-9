6111181fcddd9c869899b8983edea2b21ff5bb44
arunabh15091989 <Arunabh.social@gmail.com>
2017-06-23 14:52:17 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,6 @@
-
+import re
+def validate_email(string):
+    exp = "(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    if string != '':
+        m = re.findall(exp,string)
+    return True   
