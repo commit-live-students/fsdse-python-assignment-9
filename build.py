b763ce48a1bd22b302ca54f8dc0a9888d8cf6f60
birajparikh16 <birajparikh16@gmail.com>
2017-06-24 18:38:41 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,12 @@
+# Validate email
+import re
 
+def validate_email(email):
+    pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+# validate_email('info@greyatom.co.in')
+# Output : True
