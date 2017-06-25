3213f1f128136d8c048e629b34825f277363f57a
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-06-25 06:37:19 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,10 @@
+import re
+def validate_email(string):
+    if (string != ""):
+        if re.findall('@',string):
+            return True
+        else:
+            return False
 
+    else:
+        print " invalid email"
