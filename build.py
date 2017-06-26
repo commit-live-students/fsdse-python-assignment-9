c53235f11f45cde2c635321e6efa2d1efd586ac8
nikhilmborkar <nikhilmukundborkar@gmail.com>
2017-06-26 05:43:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,9 @@
-
+import re
+def validate_email(email_string):
+    pattern = re.compile(r"[0-9a-zA-Z_+\.]+@[0-9a-zA-Z_+\.]+\.[0-9a-zA-Z_+\.]+")
+    if re.match(pattern,email_string):
+        return True
+    if email_string == '':
+        raise ValueError
+    else:
+        return False
