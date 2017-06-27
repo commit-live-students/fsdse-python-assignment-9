614bc22f911ea3c47ae55856f4f6127605733efb
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-27 13:31:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,12 @@
+import re
 
+def validate_email(email):
+
+    EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+    if not EMAIL_REGEX.match(email):
+        return False
+    else :
+        return True
+
+validate_email("we@aregreat.com")
