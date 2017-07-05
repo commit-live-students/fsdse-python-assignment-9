c2a93c1dab50287cf7dfe148dff802711a4294a0
tejesh95 <tejeshpapineni95@gmail.com>
2017-07-05 05:15:20 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,8 @@
+import re
+def validate_email(email):
+    if email == '' or email == None:
+        return False
 
+    if re.match(r"[^@]+@[^@]+\.[^@]", email) is None:
+        return False
+    return True
