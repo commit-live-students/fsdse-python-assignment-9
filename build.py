c749c2080982017da1361e13294af92bda66a6d0
sandiprb <baradiya.sandip@gmail.com>
2017-08-21 16:23:31 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,10 @@
+import re
 
+def validate_email(email):
+    if not email:
+        return
+    email_regex = re.compile('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+    if email_regex.match(email):
+        return True
+    else:
+        return False
