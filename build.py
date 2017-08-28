6a76bbf5186dc20f03ff05cd69e2774a6fd7ba0d
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-28 13:34:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,1 +1,15 @@
+import re
 
+def validate_email(emailid):
+    match=re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]*\.*[com|org|edu]{3}$)",emailid)
+    if match:
+        return True
+    else:
+        return False
+
+
+
+'''
+a = raw_input('Enter EmailId : ')
+print validate_email(a)
+'''
